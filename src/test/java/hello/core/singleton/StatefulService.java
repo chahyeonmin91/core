7dc0837e7e57ed9b535a8fdb5236e2cc1065ea71
Chahyeon Min <chahyeonmin91@gmail.com>
2025-05-07 13:52:53 +0900

    statefulservice

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -1,14 +1,12 @@
 package hello.core.singleton;
 
 public class StatefulService {
-    private int price; //상태를 유지하는 필드
+    //싱글톤은 무상태로 만들어야함
+    //private int price; //상태를 유지하는 필드
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name=" +name +"price="+price);
-        this.price = price;
-    }
-
-    public int getPrice() {
+        //this.price = price;
         return price;
     }
 }
