4611bdaa2425d6e835a4c78dcccde9f6c6c2c84f
chahyeon min <chahyeonmin91@gmail.com>
2025-05-12 23:55:08 +0900

    basePackage

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
+
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
+        //요즘은 패키지 위치를 지정하지 않고  설정 정보 클래수ㅡ의 위치(com hello
+        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 
