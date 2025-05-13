4347f590118d89ccb8e33b59bd7146b084f609de
chahyeon min <chahyeonmin91@gmail.com>
2025-05-13 23:50:33 +0900

    componentfilter

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.FilterType;
 
         basePackages = "hello.core.member",
         basePackageClasses = AutoAppConfig.class,
-        //요즘은 패키지 위치를 지정하지 않고  설정 정보 클래수ㅡ의 위치(com hello
+        //요즘은 패키지 위치를 지정하지 않고 설정 정보 클래스의 위치를 최상단에 둔다(ex)com.hello
         excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
---
+++ src/test/java/hello/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,32 @@
+package hello.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.hibernate.annotations.Filter;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        Assertions.assertThat(beanA).isNotNull();
+
+        ac.getBean("beanB", BeanB.class);
+       // org.junit.jupiter.api.Assertions.assertThrows()
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class)
+    )
+    static class ComponentFilterAppConfig {
+    }
+}
