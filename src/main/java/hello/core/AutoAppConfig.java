16c5687f413f2e92994c0605a9c982ca6658a542
chahyeon min <chahyeonmin91@gmail.com>
2025-05-20 23:03:22 +0900

    field

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,8 +13,11 @@ import org.springframework.context.annotation.FilterType;
         basePackages = "hello.core.member",
         basePackageClasses = AutoAppConfig.class,
         //요즘은 패키지 위치를 지정하지 않고 설정 정보 클래스의 위치를 최상단에 둔다(ex)com.hello
-        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
